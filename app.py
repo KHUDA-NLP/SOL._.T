ddecc4a13d19805c99631f435038e9001cf33d02
JOONHYUk_LEE <leejoon2067@gmail.com>
2024-05-15 21:35:34 +0900

    feat: baseline streamlit code add

--- app.py
+++ app.py
@@ -1,3 +1,44 @@
 import streamlit as st
+from utils import print_messages
+from langchain_core.messages import ChatMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate # MessagePlaceHolder
+from langchain_core.output_parsers import StrOutputParser
+import os
 
-st.title("안녕하세요!! 여러분의 연애 솔루션 챗봇 SOL-T입니다!")
+from dotenv import load_dotenv
+
+st.set_page_config(
+    page_title = "연애 솔루션 챗봇 Sol-T",
+    page_icon = "SOL-T💞")
+st.title("연애 솔루션 챗봇 SOL-T💞")
+
+# API key 설정
+load_dotenv()
+os.environ.get("OPENAI_API_KEY")
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+# 이전 대화 기록 출력해주는 함수. 
+print_messages()
+
+if user_input := st.chat_input("어떤 것이 궁금하신가요?"):
+    # 사용자 입력 
+    st.chat_message("user").write(f"{user_input}")
+    st.session_state["messages"].append(ChatMessage(role = "user", content = user_input))
+
+    # LLM 답변 생성
+    prompt = ChatPromptTemplate.from_template(
+        """질문에 대해 간결하지만 최대한 친절하게 답변하라.     
+{question}
+""")
+
+    chain = prompt | ChatOpenAI() | StrOutputParser()
+    msg = chain.invoke({"question" : user_input})
+
+    # AI 답변   
+    with st.chat_message("assistant"):
+        # msg = f" '아!! {user_input}' 이렇게 답변하셨군요"
+        st.write(msg)
+        st.session_state["messages"].append(ChatMessage(role = "assistant", content = msg))
---
+++ utils/utils.py
@@ -0,0 +1,6 @@
+import streamlit as st
+#ver1. ChatMessage 사용
+def print_messages():
+    if "messages" in st.session_state and len(st.session_state["messages"]) > 0:
+        for chat_message in st.session_state["messages"]:
+            st.chat_message(chat_message.role).write(chat_message.content)
